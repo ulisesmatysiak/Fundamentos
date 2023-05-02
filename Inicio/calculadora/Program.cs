e3ed3fd180e2261962e82979289c0eaaa2219ad1
ulises matysiak <ulisesmatysiak2gmail.com>
2023-05-02 17:19:34 -0300

    group

---
+++ Inicio/calculadora/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // paso 0: declarar variables
+            // tipo de dato: int float char bool
+            int n1, n2, resultado;
+
+            // paso 1: pedir valores
+            Console.WriteLine("Ingrese un numero: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            // paso 2: realizar calculo
+            // + - * / %
+            resultado = n1 + n2;
+
+            // paso 3: emitir resultado
+            Console.WriteLine("El resultado es: " + resultado);
+        }
+    }
+}
